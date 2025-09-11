9cd651cc80bd4f2c3a4b22956645a3985dc62240
hs-matthew <matthew@headspace.media>
2025-09-11 12:19:12 -0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAC2F_pJ-pLJJfMoZ27BpMvEFJjOcqvYmo",
+  authDomain: "approval-platform.firebaseapp.com",
+  projectId: "approval-platform",
+  storageBucket: "approval-platform.firebasestorage.app",
+  messagingSenderId: "369801713653",
+  appId: "1:369801713653:web:cb12850f950ba48adb72bc",
+  measurementId: "G-YSS476SRLJ"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
