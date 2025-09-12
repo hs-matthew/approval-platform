4bc24d4577a427f4e37f5c5f49f7e2a843928d52
hs-matthew <matthew@headspace.media>
2025-09-12 00:02:02 -0400

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,1 +1,17 @@
+import { useState } from 'react';
 
+export const useAuth = () => {
+  const [currentUser] = useState({
+    id: 1,
+    name: "Admin User",
+    email: "admin@company.com",
+    role: "admin"
+  });
+
+  return {
+    currentUser,
+    isAdmin: currentUser.role === 'admin',
+    isClient: currentUser.role === 'client',
+    isWriter: currentUser.role === 'writer'
+  };
+};
