c06333a9e16834500047778043484b67959b9c42
hs-matthew <matthew@headspace.media>
2025-09-12 17:57:45 -0400

    Update ProtectedLayout.js - add background

--- src/components/layout/ProtectedLayout.js
+++ src/components/layout/ProtectedLayout.js
@@ -7,11 +7,19 @@ import { useAuth } from "../../hooks/useAuth";
 
 export default function ProtectedLayout() {
   const { currentUser } = useAuth();
+
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
+      {/* Top Nav */}
       <Navigation currentUser={currentUser} />
-      <Outlet />
-      <Footer />   {/* ✅ footer is back on all protected routes */}
-    </>
+
+      {/* Main content with light gray background */}
+      <main className="flex-grow bg-gray-100">
+        <Outlet />
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
   );
 }
