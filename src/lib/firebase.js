c48205002268e0c9be2c69eabb3bc0aa51b9723d
hs-matthew <matthew@headspace.media>
2025-09-13 13:30:46 -0400

    Update firebase.js - storage

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from "firebase/storage";
 import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 const firebaseConfig = {
@@ -21,6 +22,8 @@ export const auth = getAuth(app);
 
 // ✅ Firestore
 export const db = getFirestore(app);
+
+export const storage = getStorage(app);
 
 // ✅ App Check with reCAPTCHA v3
 initializeAppCheck(app, {
