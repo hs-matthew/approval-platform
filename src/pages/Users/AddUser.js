eb8f787ef1689a75bff8d5f402784963f4b72762
hs-matthew <matthew@headspace.media>
2025-09-13 09:31:22 -0400

    Update AddUser.js

--- src/pages/Users/AddUser.js
+++ src/pages/Users/AddUser.js
@@ -1,1 +1,29 @@
-test
+// src/pages/Users/AddUser.js
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useFirestore } from "../../hooks/useFirestore";
+import UserForm from "./UserForm";
+import { addUserWithInvite } from "./usersActions";
+
+export default function AddUser() {
+  const navigate = useNavigate();
+  const { data: users = [] } = useFirestore("users");
+  const { data: workspaces = [] } = useFirestore("workspaces");
+
+  async function handleAddUser(userData) {
+    await addUserWithInvite(userData); // writes users + invites
+    navigate("/users");
+  }
+
+  return (
+    <div className="max-w-3xl mx-auto p-6">
+      <UserForm
+        users={users}
+        workspaces={workspaces}
+        onAddUser={handleAddUser}
+        onCancel={() => navigate("/users")}
+        showTitle
+      />
+    </div>
+  );
+}
